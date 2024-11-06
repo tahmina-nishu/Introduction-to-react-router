6cb964484a14b2ad11768a29dc174304ed07b6c9
tahmina-nishu <akthertahmina572@gmail.com>
2024-11-06 08:53:46 +0600

    navigation onClick

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const User = ({user}) => {
     const {id, name, email, phone} = user;
+
+    const navigate = useNavigate();
 
     const userStyle = {
         border: '2px solid yellow',
@@ -9,12 +11,20 @@ const User = ({user}) => {
         borderRadius: '10px',
         margin: '10px'
     }
+
+    // function to handle users details 
+    const handleShowDetails = ()=> {
+        navigate(`/users/${id}`);
+    }
+
+
     return (
         <div style={userStyle}>
             <p>Name : {name}</p>
             <p>Email : {email}</p>
             <p>Phone : {phone}</p>
             <Link to={`/users/${id}`}>Show Details</Link>
+            <button onClick={handleShowDetails}>See Details</button>
         </div>
     );
 };
